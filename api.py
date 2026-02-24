c280950a4792768f7bffb38e8d74baea0fe123d9
Aliev222 <muhamadrasulaliev3489@gmail.com>
2026-02-24 05:58:21 +0300

    Исправил порт для Render

--- api.py
+++ api.py
@@ -269,6 +269,6 @@ if __name__ == "__main__":
     # Создаем таблицы
     asyncio.run(init_db())
     # Получаем порт из переменной окружения (для Render) или используем 8000 локально
-    port = int(os.environ.get("PORT", 8000))
+    #port = int(os.environ.get("PORT", 8000))
     # Запускаем сервер
     uvicorn.run(app, host="0.0.0.0", port=port)
