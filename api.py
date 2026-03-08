fe6a4817fa75f581d8613c24d01e22fc4a8dd124
Aliev222 <muhamadrasulaliev3489@gmail.com>
2026-03-08 06:01:51 +0300

    asd

--- api.py
+++ api.py
@@ -170,7 +170,7 @@ app = FastAPI(title="Ryoho Clicker API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ryoho-eta.vercel.app", "http://localhost:3000"],
+    allow_origins=["https://spirix.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
