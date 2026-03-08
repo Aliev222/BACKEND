e42750f0083e835229bd55436733d6e818d6f6ed
Aliev222 <muhamadrasulaliev3489@gmail.com>
2026-03-08 06:59:48 +0300

    SSd

--- api.py
+++ api.py
@@ -170,7 +170,12 @@ app = FastAPI(title="Ryoho Clicker API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://spirix.vercel.app", "http://localhost:3000"],
+    allow_origins=[
+        "https://spirix.vercel.app",
+        "https://web.telegram.org",
+        "https://telegram.org",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
